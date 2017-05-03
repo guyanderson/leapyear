8989a80aae0e2797d3e6abba643f8d09f80c8318
guy <luckythelich@hotmail.com>
2017-05-02 18:21:38 -0700

    add tf logic for divisable by 400. dry'ed up code.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,8 @@ $(document).ready(function() {
     $("#result").text(result);
   });
     var leapYear = function(year) {
-      if (year % 100 === 0) {
-       return false;
-     } else if (year % 4 === 0) {
-       return true;
+      if ((year % 4 === 0) && (year %100 !== 0) || (year % 400 === 0)) {
+        return true;
      } else {
        return false;
      }
