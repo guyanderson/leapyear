db1a7ae0adf2ffcb50b74aaaba3adbe908c35ab4
guy <luckythelich@hotmail.com>
2017-05-02 18:10:01 -0700

    check divisable by 100 = false.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
     $("#result").text(result);
   });
     var leapYear = function(year) {
-      if (year % 4 === 0) {
+      if (year % 100 === 0) {
+       return false;
+     } else if (year % 4 === 0) {
        return true;
      } else {
        return false;
